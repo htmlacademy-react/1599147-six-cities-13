ae035283f066c18f55be3166ae4634871846749f
dimpulsha <dimpulsha@gmail.com>
2023-07-13 00:25:48 +0500

    добавлены маршруты приложения

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,9 @@
 import { AppRoute } from '../../constants/app-routes';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import OfferPage from '../../pages/offer-page/offer-page';
 import { CitiesListType } from '../../types/cities-types';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -16,7 +20,12 @@ export default function App(props: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<MainPage placesCount={pageCardCount} offerCount={offerByCityCount} cityList={cityList}/> } />
+        <Route path={AppRoute.Main} element={<MainPage placesCount={pageCardCount} offerCount={offerByCityCount} cityList={cityList} />} />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Offer} element={<OfferPage />} />
+        <Route path={AppRoute.Favorites} element={<FavoritesPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+
       </Routes>
     </BrowserRouter>
 
