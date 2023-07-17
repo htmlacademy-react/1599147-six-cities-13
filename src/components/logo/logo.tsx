d25fddf007403a4d167736980e182c5ab0b070fb
dimpulsha <dimpulsha@gmail.com>
2023-07-17 21:50:03 +0500

    подключает стили для страницы not-found

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -3,7 +3,7 @@ import { AppRoute } from '../../constants/app-routes';
 
 export default function Logo(): JSX.Element {
   return (
-    /*header__logo-link--active*/
+    /*header__logo-link--active для main, чтобы не меняла внешний вид */
     <Link to={AppRoute.Root} className="header__logo-link">
       <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
     </Link>
--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,19 +1,20 @@
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import './not-found-page.css';
 
 export default function NotFoundPage(): JSX.Element {
   return (
-    <div className="page page--gray page--main">
+    <div className="page page--not-found">
       <Header />
-      <main className="page__main page__main--favorites page__main--favorites-empty">
+      <main className="page__main page__main--not-found page__main--favorites-empty">
         <div className="page__favorites-container container">
-          <section className="favorites favorites--empty">
+          <section className="not-found-text ">
             <h1 className="visually-hidden">404 - Page not found</h1>
-            <div className="favorites__status-wrapper">
-              <b className="favorites__status">404</b>
+            <div className="not-found__wrapper">
+              <b className="not-found__status">404</b>
               <br/>
-              <p className="favorites__status-description">A page is not found</p>
-              <p className="favorites__status-description">Perhaps you are tired. Try logging off, taking a break and then entering the address again</p>
+              <p className="not-found__status-description">A page is not found</p>
+              <p className="not-found__status-description">Perhaps you are tired. Try logging off, taking a break and then entering the address again</p>
             </div>
           </section>
         </div>
