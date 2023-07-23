536a5c4eac36694cd7b59fa2328c7eca64813e3d
dimpulsha <dimpulsha@gmail.com>
2023-07-23 16:58:19 +0500

    исправление ошибки линтера

--- src/components/new-review/new-review.tsx
+++ src/components/new-review/new-review.tsx
@@ -5,7 +5,7 @@ export default function NewReview(): JSX.Element {
 
   const ratingList = Object.entries(RatingList).reverse();
 
-  const [rating, setRating] = useState('0');
+  const [, setRating] = useState('0');
 
   const [commentText, setCommentText] = useState('');
 
