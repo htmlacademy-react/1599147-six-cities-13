b0c87d2c346301b92c46b0e6d8214e9b51dc182a
dimpulsha <dimpulsha@gmail.com>
2023-08-01 23:13:35 +0500

    редъюсер для загрузки списка офферов

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,4 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { CityItemType } from '../types/cities-types';
+import { OfferListType } from '../types/offer-types';
 
 export const changeCity = createAction<{ city: CityItemType | undefined}>('offer/changeCity');
+export const loadOfferList = createAction<{ offerList: OfferListType }>('offer/loadOfferList');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,10 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { DEFAULT_CITY } from '../constants/cities-list';
 import { OfferListType } from '../types/offer-types';
-import { changeCity } from './action';
+import { changeCity, loadOfferList } from './action';
 import { CityItemType } from '../types/cities-types';
+
+import mockOfferList from '../mocks/offer-list.json';
 
 type OfferListState = {
   city: CityItemType | undefined;
@@ -18,5 +20,8 @@ export const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(changeCity, (state, action) => {
       state.city = action.payload.city;
+    })
+    .addCase(loadOfferList, (state) => {
+      state.offerList = mockOfferList;
     });
 });
